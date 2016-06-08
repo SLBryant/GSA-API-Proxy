cb4f6da4d83698145eaff1fa126f74f7312e2093
Connor <connor.franklin@ogilvy.com>
2016-06-08 14:46:59 -0400

    support https on dev.oagov.com

--- server.js
+++ server.js
@@ -4,16 +4,25 @@ var request = require('request');
 var express = require('express');
 var app = express();
 
+SSLCertificateFile    /etc/letsencrypt/live/dev.oagov.com/cert.pem
+        SSLCertificateKeyFile /etc/letsencrypt/live/dev.oagov.com/privkey.pem
+
+var options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/dev.oagov.com/cert.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/dev.oagov.com/privkey.pem')
+};
+
+
 var apiUrl = 'http://gsa.gov';
 
-app.use('/', function(req, res) {
+app.use('/proxy/', function(req, res) {
     var url = apiUrl + req.url;
     request({
     	uri: url
     }, function(error, response, html) {
     	if (!error && response.statusCode == 200) {
-            var json = JSON.parse(html)
-        	res.header('Access-Control-Allow-Origin', '*').send(json);
+		var json = JSON.parse(html)
+        	res.header('Access-Control-Allow-Origin', '*').json(json);
     	}
     	else{
     		res.header('Access-Control-Allow-Origin', '*').status(404).send('404');
@@ -21,4 +30,7 @@ app.use('/', function(req, res) {
     })
 });
 
-app.listen(process.env.PORT || 3334);
+// Create an HTTP service.
+http.createServer(app).listen(80);
+// Create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(3334);
